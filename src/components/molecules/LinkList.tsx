689fb54dd16994fd809bf9a973a1fe16b705d7aa
Murat Akkus <akkusmurat123@gmail.com>
2025-01-16 21:16:55 +0100

    New Link list created and maked functional.

--- src/components/molecules/LinkList.tsx
+++ src/components/molecules/LinkList.tsx
@@ -3,23 +3,30 @@ import { RootState } from '@/store/store';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { DocumentData } from 'firebase-admin/firestore';
+import LinkListItem from '../atoms/LinkListItem';
 
 function LinkList() {
   const userData = useSelector((store: RootState) => store.onAuthChangeState);
   const [listData, setListData] = useState<DocumentData[]>([]);
 
   useEffect(() => {
-    console.log(listData);
     const getData = async () => {
       if (userData.companyId) {
         const data = await getAllRegisterLinks(userData.companyId);
+        console.log(data[0]);
         setListData(data);
       }
     };
     getData();
   }, [userData]);
 
-  return <div className="w-full flex-1 rounded-lg"></div>;
+  return (
+    <div className="w-full flex-1 rounded-lg flex flex-col gap-2 pb-4 md:gap-4 overflow-y-auto">
+      {listData.map((item, key) => (
+        <LinkListItem key={key} link={item.link} isValid={item.isValid} />
+      ))}
+    </div>
+  );
 }
 
 export default LinkList;
