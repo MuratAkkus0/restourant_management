ade680c66bfdd35bd2b7f51fb27185e83e276d1d
Murat Akkus <kkusmurat123@gmail.com>
2025-01-18 18:06:37 +0100

    personal list route added

---
+++ src/views/admin/AdminPanelView.tsx
@@ -0,0 +1,19 @@
+import { Outlet, useNavigate } from 'react-router-dom';
+import DashMenu from '../../components/organisms/dashboard/DashMenu';
+import { useEffect } from 'react';
+
+function AdminPanelView() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const url = window.location.href.split('/').pop();
+    if (url === 'admin') navigate('overview');
+  }, []);
+  return (
+    <div className="w-full h-full flex pl-4 md:pl-0 md:gap-5">
+      <DashMenu />
+      <Outlet />
+    </div>
+  );
+}
+
+export default AdminPanelView;
