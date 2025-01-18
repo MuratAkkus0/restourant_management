930690b0b86aa9a8e1cab6bcc357152627867fac
Murat Akkus <kkusmurat123@gmail.com>
2025-01-18 17:56:15 +0100

    Unnecessary files deleted .

--- src/views/admin/AdminPersonalListView.tsx
+++
@@ -1,7 +0,0 @@
-import AdminPanelsPagesContainer from '../../components/templates/AdminPanelsPagesContainer';
-
-function AdminPersonalListView() {
-  return <AdminPanelsPagesContainer>AdminPersonalView</AdminPanelsPagesContainer>;
-}
-
-export default AdminPersonalListView;
