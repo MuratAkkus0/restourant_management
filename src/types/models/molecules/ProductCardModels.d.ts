5d328a634cb3879e8bf9befd382fe66184bd256d
Murat Akkus <akkusmurat123@gamil.com>
2025-02-05 22:07:18 +0100

    Product price prop added to type

--- src/types/models/molecules/ProductCardModels.d.ts
+++ src/types/models/molecules/ProductCardModels.d.ts
@@ -4,6 +4,7 @@ export interface ProductCardProps {
   cardImgSrc: string;
   cardTitle: string;
   cardDetails: string;
+  cardPrice: number;
   children: React.ReactNode;
   onDelBtnClick: MouseEventHandler<HTMLDivElement>;
 }
